1a00bb7c98bb27f6ab90808153a456ecc2ad1abf
Zajíc Jan <zajic@vejhybka.cz>
2019-05-11 07:36:46 +0200

    Přidejte soubory projektu.

---
+++ P190012_Creasoft/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P190012_Creasoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
